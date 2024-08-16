4938a3a24ea9ccc04ebe3faa26f37c26e06c0d28
doaa102938 <doaa102938@gmail.com>
2024-08-16 18:02:41 +0300

    product2

--- src/Pages/Women/Women.jsx
+++ src/Pages/Women/Women.jsx
@@ -1,22 +1,27 @@
 
 import { useEffect, useState } from "react";
 import ProductItem from "../../Components/ProductItem/ProductItem";
+import image1 from '../../assets/Images/image-product-1.jpg'
+import image2 from '../../assets/Images/image-product-2.jpg'
+import image3 from '../../assets/Images/image-product-3.jpg'
+import image4 from '../../assets/Images/image-product-4.jpg'
 
 
 function Women() {
     const [product, setProduct] = useState([
         {
-            "id": 1,
-            "productName": "Sneaker company",
-            "Title": "Fall Limited Edition Sneakers",
-            "Description": "These low-profile sneakers are perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.",
-            "Total": "$125.00",
-            "Discount": "50%",
-            "price": "$250.00",
-            "ProductPhoto1": "src/assets/Images/image-product-1.jpg",
-            "ProductPhoto2": "src/assets/Images/image-product-2.jpg",
-            "ProductPhoto3": "src/assets/Images/image-product-3.jpg",
-            "ProductPhoto4": "src/assets/Images/image-product-4.jpg"
+            id: 1,
+            productName: "Sneaker company",
+            Title: "Fall Limited Edition Sneakers",
+            Description: "These low-profile sneakers are perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.",
+            Total: "$125.00",
+            Discount: "50%",
+            price: "$250.00",
+            ProductPhoto1: image1,
+            ProductPhoto2: image2,
+            ProductPhoto3: image3,
+            ProductPhoto4: image4,
+
         }
 
 
